e319342b268de08b7cf817ee02678df753d984a7
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-16 17:25:26 +0530

    JS86

---
+++ JS86.js
@@ -0,0 +1,16 @@
+let a = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(49);
+    }, 4000);
+  });
+};
+
+(async () => {
+  let b = await a();
+  console.log(b);
+  let c = await a();
+  console.log(c);
+  let d = await a();
+  console.log(d);
+})();

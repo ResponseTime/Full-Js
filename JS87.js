7afbcaca78c50c74e84cd5621c29054ad4a7748f
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-17 16:33:52 +0530

    JS87

---
+++ JS87.js
@@ -0,0 +1,25 @@
+// let arr = [21, 54];
+// let [a, b] = arr;
+// console.log(a, b);
+
+// let arr = [32, 64, 2, 3, 5];
+// let [a, , c, ...rest] = arr;
+// console.log(a, c, rest);
+
+// const obj = {
+//   name: "nigg",
+//   age: 23,
+// };
+
+// let { name } = obj;
+// console.log(name);
+// let ar = [234, 5, 23, 4];
+// let arr = [1, 2, 3, ...ar];
+
+// console.log(arr);
+// let arr = [21, 54, 31];
+// let e = (a) => {
+//   return a ** 2;
+// };
+// let k = arr.map((a) => e(a));
+// console.log(k);

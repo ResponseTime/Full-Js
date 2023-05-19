370c91d25b3a098efe8365bfe6e1b2a8dc71cf35
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-19 16:49:52 +0530

    JS88,89

---
+++ JS88.js
@@ -0,0 +1,7 @@
+// let p = 23;
+// {
+//   let a = 4;
+//   console.log(a);
+//   console.log(p);
+// }
+// console.log(a);
---
+++ JS89.js
@@ -0,0 +1,12 @@
+// greet();
+
+// function greet() {
+//   console.log("Hello World"); -> function declaration will work
+// }
+// let greet = () => {
+//   console.log("Hello World"); -> function expression will not work
+// };
+
+// console.log(a);
+// var a = 2;
+// console.log(a);

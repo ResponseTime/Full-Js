e1184391ad8b21e6bd057bb52af3ed1c69f735ee
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-24 17:37:47 +0530

    JS95

---
+++ JS95.js
@@ -0,0 +1,10 @@
+// const fs = require("fs").promises;
+// let wr = async () => {
+//   await fs.writeFile("my.txt", "Hello World", { flag: "a" });
+// };
+// let fn = async () => {
+//   const res = await fs.readFile("my.txt", "utf-8");
+//   console.log(res);
+// };
+// fn();
+// wr();

aeead7d43ed184eb47f31a231e885dc14f58424e
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-26 10:24:25 +0530

    JS96,97

---
+++ JS96.js
@@ -0,0 +1,3 @@
+// npm init - make new node project
+// npm install {module_name} --save
+// npm install - installs all dependencies in package.json
---
+++ JS97.js
@@ -0,0 +1,27 @@
+// common js modules
+// module1.js{
+//     const a = ()=>{
+// console.log("Hello World");
+// }
+// module.exports = a;
+// }
+// app.js{
+//     const a = require('./module1.js');
+//     console.log(a);
+// }
+
+// ES6 modules
+// module1.js{
+// export const a = ()=>{
+// console.log("Hello World");
+// }
+//
+// }
+
+// app.js{
+// import {a} from './module1.js';
+// console.log(a);
+// }
+
+// type: module for es6 modules
+// export default {}
